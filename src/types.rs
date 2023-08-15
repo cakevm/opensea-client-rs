e1154fac85e0fe58a1e9494ca4e27c28f4d07405
Zero Hero <0xzerohero@gmail.com>
2023-08-15 19:47:58 +0300

    Add Chain struct

--- src/types.rs
+++ src/types.rs
@@ -2,8 +2,9 @@ pub mod api;
 
 use super::constants::{API_BASE_MAINNET, API_BASE_TESTNET};
 use crate::constants::PROTOCOL_VERSION;
-use ethers::types::Chain;
+use serde::{Deserialize, Serialize};
 use std::fmt;
+use strum::{AsRefStr, EnumString};
 
 /// API endpoints
 pub enum ApiUrl {
@@ -35,5 +36,98 @@ impl ApiUrl {
 
     pub fn fulfill_listing(&self) -> String {
         format!("{}/listings/fulfillment_data", self.base())
+    }
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize, EnumString, AsRefStr, Default)]
+#[serde(rename_all = "snake_case")]
+#[strum(serialize_all = "snake_case")]
+pub enum Chain {
+    // Mainnet Chains
+    #[default]
+    #[strum(to_string = "ethereum", serialize = "mainnet")]
+    #[serde(alias = "mainnet")]
+    Ethereum,
+    #[strum(to_string = "matic", serialize = "polygon")]
+    #[serde(rename = "matic", alias = "polygon")]
+    Polygon,
+    Klaytn,
+    Base,
+    BSC,
+    Arbitrum,
+    ArbitrumNova,
+    Avalanche,
+    Optimism,
+    Solana,
+    Zora,
+
+    // Testnet Chains
+    Goerli,
+    Sepolia,
+    Mumbai,
+    Boabab,
+    BaseGoerli,
+    BSCTestnet,
+    ArbitrumGoerli,
+    #[strum(to_string = "avalanche_fuji", serialize = "fuji")]
+    #[serde(alias = "fuji")]
+    AvalancheFuji,
+    OptimismGoerli,
+    SolanaDevnet,
+    ZoraTestnet,
+}
+
+impl fmt::Display for Chain {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.pad(self.as_ref())
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use serde_json::Value;
+
+    #[test]
+    fn can_display_and_parse_chain() {
+        let chain = Chain::Polygon;
+        assert_eq!(format!("{chain}"), "matic");
+
+        let chain: Chain = Default::default();
+        assert_eq!(format!("{chain}"), "ethereum");
+
+        let chain = Chain::AvalancheFuji;
+        assert_eq!(format!("{chain}"), "avalanche_fuji");
+
+        let chain: Chain = "polygon".parse().unwrap();
+        assert_eq!(chain, Chain::Polygon);
+    }
+
+    #[test]
+    fn can_serialize_chain() {
+        let chain = Chain::Polygon;
+        let value = serde_json::to_value(&chain).unwrap();
+        assert_eq!(Value::String("matic".to_string()), value);
+
+        let chain: Chain = Default::default();
+        let value = serde_json::to_value(&chain).unwrap();
+        assert_eq!(Value::String("ethereum".to_string()), value);
+    }
+
+    #[test]
+    fn can_deserialize_chain() {
+        #[derive(Deserialize)]
+        struct ChainTest {
+            chain: Chain,
+        }
+
+        let data: ChainTest = serde_json::from_str(r#"{ "chain": "matic" }"#).unwrap();
+        assert_eq!(data.chain, Chain::Polygon);
+
+        let data: ChainTest = serde_json::from_str(r#"{ "chain": "mainnet" }"#).unwrap();
+        assert_eq!(data.chain, Chain::Ethereum);
+
+        let data: ChainTest = serde_json::from_str(r#"{ "chain": "ethereum" }"#).unwrap();
+        assert_eq!(data.chain, Chain::Ethereum);
     }
 }
